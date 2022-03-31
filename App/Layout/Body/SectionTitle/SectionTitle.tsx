05cbcff45913b383f4143bbe3de6921a3cb1f87c
Vasilii <ahmadiyarov1992@gmail.com>
2022-03-31 23:55:17 +0300

    added type animation

--- App/Layout/Body/SectionTitle/SectionTitle.tsx
+++ App/Layout/Body/SectionTitle/SectionTitle.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import codeText from '../../arrays/codeText';
 import styles from './sectiontitle.css';
 
 export function SectionTitle() {
+
+  let time: ReturnType<typeof setTimeout>;
+
+  const [str, setStr] = useState('');
+  const [strRemove, setStrRemove] = useState('');
+  const [strShowed, setStrShowed] = useState('');
+  const [index, setIndex] = useState(0);
+
+  async function inputText(text: string, str: string, constName: any, ms: number) {
+    let textArr = text.split('');
+    let currentStr = '';
+
+    if (str === text && index <= text.length) {
+      clearTimeout(constName);
+    } else {
+      constName = setTimeout(() => {
+        currentStr = str + textArr[index];
+        setStr(currentStr);
+        setIndex(index + 1)
+        setStrShowed(currentStr);
+        if (currentStr === text ) setStrRemove(currentStr);
+      }, ms);
+    }
+  };
+
+  async function removeText( strRemove: string, constName: any, ms: number) {
+    let textArr: any;
+    let currentStr = '';
+
+    if (strRemove === '' && index === 0) {
+      clearTimeout(constName);
+    } else {
+      constName = setTimeout(() => {
+          textArr = strRemove.split('');
+          textArr.length = index;
+          currentStr = textArr.join('');
+          index > 0 ? setIndex(index - 1) : setIndex(0) ;
+          setStrRemove(currentStr);
+          setStrShowed(currentStr);
+          if (currentStr === '') setStr(currentStr);
+      }, ms);
+    }
+  };
+
+  const animText = (inputStr: string, removeStr:string, text: string) => {
+    if (inputStr !== text) {
+      inputText(text, inputStr, time, 500)
+    } else {
+      removeText(removeStr, time, 500)
+    }
+  }
+
+  // for (let el of codeText) {
+
+    animText(str, strRemove, 'Vasilii')
+  // }
+
   return (
     <section className={styles.container}>
       <svg width="1504" height="690" viewBox="0 0 1504 690" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -28,9 +86,9 @@ export function SectionTitle() {
         <div className={styles.mainContentContainer} >
           <h1 className={styles.title} >Разрабатываем профессиональные <br /> IT - решения для вашего бизнеса:</h1>
           <p className={styles.title}>
-            <span> &lt;/ </span>
-            <span></span>
-            <span>&gt;</span>
+            <span className={styles.codeAround} > &lt;/</span>
+            <span className={styles.code}>{strShowed}</span>
+            <span className={styles.codeAround}>&gt;</span>
           </p>
         </div>
       </div>
---
+++ App/Layout/arrays/codeText.ts
@@ -0,0 +1,10 @@
+const codeText = [
+        'BLOCKHAIN',
+        'САЙТЫ',
+        'ГОЛОСОВЫЕ ИНТЕРФЕЙСЫ',
+        'ВЕБ-ПРОЕКТЫ',
+        'IOT',
+        'МОБИЛЬНЫЕ ПРИЛОЖЕНИЯ'
+]
+
+export default codeText;
